0cac23a4e28416b29c37e12dcf24a86be5b23a05
drw <drw@users.noreply.github.com>
2017-08-08 15:47:34 -0400

    Work on resolving the issue of empty strings or None values being problematic key fields

--- etl/export_liens_from_csv.py
+++ etl/export_liens_from_csv.py
@@ -3,9 +3,11 @@ from marshmallow import fields, pre_load, post_load
 import pipeline as pl
 from subprocess import call
 import pprint
+import time
 #import yesterday
 
-SETTINGS_FILE = '/Users/daw165/data/TaxLiens/etl/settings-liens.json'
+#SETTINGS_FILE = '/Users/daw165/data/TaxLiens/etl/settings-liens.json'
+from local_parameters import SETTINGS_FILE
 
 class RawLiensSchema(pl.BaseSchema):
     pin = fields.String(dump_to="pin", allow_none=True)
@@ -20,7 +22,19 @@ class RawLiensSchema(pl.BaseSchema):
     amount = fields.Float(dump_to="amount", allow_none=True)
     party_type = fields.String(dump_to="party_type", allow_none=True)
     party_name = fields.String(dump_to="party_name", allow_none=True)
-    assignee = fields.String(allow_none=True)
+    assignee = fields.String(allow_none=False)
+    # Isn't it the case that 'assignee' was chosen as a field for the raw lien records because 
+    # it is the only field that can be used to differentiate some records? In that case, maybe 
+    # empty or '' assignee values should be replaced by '-' or ' ' or something. (This is based
+    # on the previous finding/suspicion that both None value and empty strings resulted in keys
+    # that would not work properly in certain cases.) And what about the case of integer fields
+    # or other types?
+
+    # [Note that since this script is taking data from CSV files, there should be no 
+    # columns with None values. It should all be instances like [value], [value],, [value],...
+    # where the missing value starts as as a zero-length string, which this script
+    # is then responsible for converting into something more appropriate.
+
 #    party_first = fields.String(dump_to="first_name", allow_none=True)
 #    party_middle = fields.String(dump_to="middle_name", allow_none=True)
     # It may be possible to exclude party_first and party_middle
@@ -32,8 +46,12 @@ class RawLiensSchema(pl.BaseSchema):
     class Meta:
         ordered = True
 
+    # From the Marshmallow documentation:
+    #   Warning: The invocation order of decorated methods of the same 
+    #   type is not guaranteed. If you need to guarantee order of different 
+    #   processing steps, you should put them in the same processing method.
     @pre_load
-    def omit_owners(self, data):
+    def omit_owners_and_avoid_null_keys(self, data):
         if data['party_type'] == 'Property Owner':
             data['party_type'] = '' # If you make these values
             # None instead of empty strings, CKAN somehow
@@ -49,6 +67,27 @@ class RawLiensSchema(pl.BaseSchema):
         data['assignee'] = str(data['party_name'])
         del data['party_type']
         del data['party_name']
+    # The stuff below was originally written as a  separate function 
+    # called avoid_null_keys, but based on the above warning, it seems 
+    # better to merge it with omit_owners.
+        if data['assignee'] is None:
+    #        data['assignee'] = ' '
+            pprint.pprint(data)
+            raise ValueError("Found a null value for 'assignee'")
+        if data['dtd'] is None:
+    #        data['dtd'] = ' '
+            pprint.pprint(data)
+            raise ValueError("Found a null value for 'dtd'")
+        if data['lien_description'] is None:
+    #        data['lien_description'] = ' '
+            pprint.pprint(data)
+            raise ValueError("Found a null value for 'lien_description'")
+        if data['tax_year'] is None:
+    #        data['tax_year'] = 0
+            pprint.pprint(data)
+            raise ValueError("Found a null value for 'tax_year'")
+
+
 
     @pre_load
     def fix_date(self, data):
@@ -63,7 +102,8 @@ class RawLiensSchema(pl.BaseSchema):
                     try:
                         data['filing_date'] = datetime.datetime.strptime(data['filing_date'], "%d-%b-%y").date().isoformat()
                     except:
-                        # Try the format I got in one instance when I exported the data from CKAN and then reimported it:
+                        # Try the format I got in one instance when I exported the 
+                        # data from CKAN and then reimported it:
                         data['filing_date'] = datetime.datetime.strptime(data['filing_date'], "%Y-%m-%d").date().isoformat()
         else:
             print("No filing date for {} and data['filing_date'] = {}".format(data['dtd'],data['filing_date']))
@@ -84,17 +124,28 @@ class RawLiensSchema(pl.BaseSchema):
 #the referenced settings.json file when the corresponding
 #flag below is True.
 def main():
-    #resource_name = 'Raw tax-lien records (beta 7)'
-    resource_id = '8cd32648-757c-4637-9076-85e144997ca8' # Raw liens
-    target = '/Users/daw165/data/TaxLiens/July31_2013/raw-liens.csv'
+    resource_name = 'Raw tax-lien records to present (beta)'
+    #resource_id = '8cd32648-757c-4637-9076-85e144997ca8' # Raw liens
+    #target = '/Users/daw165/data/TaxLiens/July31_2013/raw-liens.csv' # This path is hard-coded.
+    target = '/Users/drw/WPRDC/Tax_Liens/lien_machine/testing/raw-liens-test.csv' # This path is also hard-coded.
     log = open('uploaded.log', 'w+')
 
     #test = yesterday.run()
     #if not test:
     #    exit(0)
 
+    server = "production"
+    # Code below stolen from prime_ckan/*/open_a_channel() but really from utility_belt/gadgets
+    with open(os.path.dirname(os.path.abspath(__file__))+'/ckan_settings.json') as f: # The path of this file needs to be specified.
+        settings = json.load(f)
+    site = settings['loader'][server]['ckan_root_url']
+    package_id = settings['loader'][server]['package_id']
+
+    print("Preparing to pipe data from {} to package ID {} on {}".format(target,package_id,site))
+    time.sleep(1.0)
+
     lien_and_mean_pipeline = pl.Pipeline('lien_and_mean_pipeline',
-                                      'Tax Liens Pipeline',
+                                      'Raw Tax Liens Pipeline',
                                       log_status=False,
                                       settings_file=SETTINGS_FILE,
                                       settings_from_file=True,
@@ -108,7 +159,7 @@ def main():
               #package_id=package_id,
               resource_id=resource_id,
               #resource_name=resource_name,
-              key_fields=['dtd','lien_description','tax_year','pin', 'assignee'],
+              key_fields=['dtd','lien_description','tax_year','pin','assignee'],
               # A potential problem with making the pin field a key (is that one property
               # could have two different PINs (due to the alternate PIN) though I
               # have gone to some lengths to avoid this.)
