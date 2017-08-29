017dd328591b28cd8970c85950dc1f942897791d
drw <drw@users.noreply.github.com>
2017-08-29 14:39:31 -0400

    Add dummy DTD strings to tests.py to test convert_blocklot_to_pin function

--- tests.py
+++ tests.py
@@ -12,21 +12,21 @@ class MyTest(unittest.TestCase):
 #        For example,
 #            558H67 => 0558-H-00067-0000-00
 
-        self.assertEqual(convert_blocklot_to_pin("1360A268"), "1360A00268000000")
-        self.assertEqual(convert_blocklot_to_pin("352B94"), "0352B00094000000")
-        self.assertEqual(convert_blocklot_to_pin("159C221"), "0159C00221000000")
-        self.assertEqual(convert_blocklot_to_pin("159D250"), "0159D00250000000")
-        self.assertEqual(convert_blocklot_to_pin("2014E81"), "2014E00081000000")
-        self.assertEqual(convert_blocklot_to_pin("233F181"), "0233F00181000000")
-        self.assertEqual(convert_blocklot_to_pin("214G46"), "0214G00046000000")
-        self.assertEqual(convert_blocklot_to_pin("558H67"), "0558H00067000000")
-        self.assertEqual(convert_blocklot_to_pin("2013J377"), "2013J00377000000")
-        self.assertEqual(convert_blocklot_to_pin("214L128"), "0214L00128000000")
-        self.assertEqual(convert_blocklot_to_pin("430M330"), "0430M00330000000")
-        self.assertEqual(convert_blocklot_to_pin("215N56"), "0215N00056000000")
-        self.assertEqual(convert_blocklot_to_pin("161P50"), "0161P00050000000")
-        self.assertEqual(convert_blocklot_to_pin("214R184"), "0214R00184000000")
-        self.assertEqual(convert_blocklot_to_pin("161S334"), "0161S00334000000")
+        self.assertEqual(convert_blocklot_to_pin("1360A268","0"), "1360A00268000000")
+        self.assertEqual(convert_blocklot_to_pin("352B94","1"), "0352B00094000000")
+        self.assertEqual(convert_blocklot_to_pin("159C221","2"), "0159C00221000000")
+        self.assertEqual(convert_blocklot_to_pin("159D250","3"), "0159D00250000000")
+        self.assertEqual(convert_blocklot_to_pin("2014E81","4"), "2014E00081000000")
+        self.assertEqual(convert_blocklot_to_pin("233F181","5"), "0233F00181000000")
+        self.assertEqual(convert_blocklot_to_pin("214G46","6"), "0214G00046000000")
+        self.assertEqual(convert_blocklot_to_pin("558H67","7"), "0558H00067000000")
+        self.assertEqual(convert_blocklot_to_pin("2013J377","8"), "2013J00377000000")
+        self.assertEqual(convert_blocklot_to_pin("214L128","9"), "0214L00128000000")
+        self.assertEqual(convert_blocklot_to_pin("430M330","10"), "0430M00330000000")
+        self.assertEqual(convert_blocklot_to_pin("215N56","11"), "0215N00056000000")
+        self.assertEqual(convert_blocklot_to_pin("161P50","12"), "0161P00050000000")
+        self.assertEqual(convert_blocklot_to_pin("214R184","13"), "0214R00184000000")
+        self.assertEqual(convert_blocklot_to_pin("161S334","14"), "0161S00334000000")
         # 8000T[0-9]* represents a trailer.
 
     def test_anomalous_blocklot_conversion(self):
@@ -36,17 +36,17 @@ class MyTest(unittest.TestCase):
 
 #            232L374 2 => 0232-L-00374-0002-00
 #                                         ^
-        self.assertEqual(convert_blocklot_to_pin("232L374 2"), "0232L00374000200")
+        self.assertEqual(convert_blocklot_to_pin("232L374 2","15"), "0232L00374000200")
         # [This turns out to be the default assumption for the original
         # script.]
 #       ... and in the fifth part in other cases
 
 #            233J211 2 => 0233-J-00211-0000-02
 #                                            ^
-        self.assertEqual(convert_blocklot_to_pin("233J211 2"), "0233J00211000002")
+        self.assertEqual(convert_blocklot_to_pin("233J211 2","16"), "0233J00211000002")
 
-        self.assertEqual(convert_blocklot_to_pin("214L167 1"), "0214L00167000100")
-        self.assertEqual(convert_blocklot_to_pin("236G99 A"), "0236G00099000A00")
+        self.assertEqual(convert_blocklot_to_pin("214L167 1","17"), "0214L00167000100")
+        self.assertEqual(convert_blocklot_to_pin("236G99 A","18"), "0236G00099000A00")
 
 
         # The tax-liens file describes the following Block Lot number as
@@ -55,14 +55,14 @@ class MyTest(unittest.TestCase):
         #   "THE SUMMIT CONDO LOT 130 X 192.80 CALIFORNIA","AVE","PT 7 STY BRK HI RISE APT BLDG UNIT 307",
         # So, condominia are considered properties, even though the owner
         # many not own the ground-floor-level data.
-        self.assertEqual(convert_blocklot_to_pin("160A154 307"), "0160A00154030700")
-        self.assertEqual(convert_blocklot_to_pin("160A132 104"), "0160A00132010400")
-        self.assertEqual(convert_blocklot_to_pin("160J60 708"), "0160J00060070800")
-        self.assertEqual(convert_blocklot_to_pin("329G20 27D"), "0329G00020027D00")
-        self.assertEqual(convert_blocklot_to_pin("58P100 T1"), "0058P0010000T100")
+        self.assertEqual(convert_blocklot_to_pin("160A154 307","19"), "0160A00154030700")
+        self.assertEqual(convert_blocklot_to_pin("160A132 104","20"), "0160A00132010400")
+        self.assertEqual(convert_blocklot_to_pin("160J60 708","21"), "0160J00060070800")
+        self.assertEqual(convert_blocklot_to_pin("329G20 27D","22"), "0329G00020027D00")
+        self.assertEqual(convert_blocklot_to_pin("58P100 T1","23"), "0058P0010000T100")
 
         # Two-digit number at the end:
-        self.assertEqual(convert_blocklot_to_pin("215J110 01"), "0215J00110000001")
+        self.assertEqual(convert_blocklot_to_pin("215J110 01","24"), "0215J00110000001")
 
 
         # Anomalous case where 99B175 21 translates to
@@ -71,17 +71,17 @@ class MyTest(unittest.TestCase):
         # look for PINs of the former shape, we may find false
         # positives and screw up our counts more. Therefore,
         # we will only do this last transformation as a last resort.
-        self.assertEqual(convert_blocklot_to_pin("307G395 99"), "0307G00395000909")
-        self.assertEqual(convert_blocklot_to_pin("99B175 21"), "0099B00175000201")
-        self.assertEqual(convert_blocklot_to_pin("10G235 21"), "0010G00235000201")
+        self.assertEqual(convert_blocklot_to_pin("307G395 99","25"), "0307G00395000909")
+        self.assertEqual(convert_blocklot_to_pin("99B175 21","26"), "0099B00175000201")
+        self.assertEqual(convert_blocklot_to_pin("10G235 21","27"), "0010G00235000201")
         # Completely anomalous and difficult to categorize:
 
         # Four alphanumeric characters after the space:
-        self.assertEqual(convert_blocklot_to_pin("236G132 A000"), "0236G00132000A00") #<== Possibly this one is a case of human error, but it has happened for many different properties (~25). All cases (which occurred between 2012 and 2014 inclusive), have been manually checked to verify that the new rule concocted to handle this case holds for all such Block Lot numbers.
+        self.assertEqual(convert_blocklot_to_pin("236G132 A000","28"), "0236G00132000A00") #<== Possibly this one is a case of human error, but it has happened for many different properties (~25). All cases (which occurred between 2012 and 2014 inclusive), have been manually checked to verify that the new rule concocted to handle this case holds for all such Block Lot numbers.
 
-        self.assertEqual(convert_blocklot_to_pin("37N210 A463"),"0037N00210A46300")
+        self.assertEqual(convert_blocklot_to_pin("37N210 A463","29"),"0037N00210A46300")
         # Four-digit number after the space:
-        self.assertEqual(convert_blocklot_to_pin("100C50 4099"), "0100C00050409900")
+        self.assertEqual(convert_blocklot_to_pin("100C50 4099","30"), "0100C00050409900")
 #   19R100 02 (from a 1995 lien) => 0019R00100000000???
 
 # 	56J104 02 => nothing.
