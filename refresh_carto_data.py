2e7db7181cc20e16ee403de1a92e08a6e569e439
drw <drw@users.noreply.github.com>
2017-10-16 14:19:14 -0400

    Partially update code to refresh a Carto dataset

--- refresh_carto_data.py
+++ refresh_carto_data.py
@@ -11,29 +11,32 @@ def user_authorize_carto():
     auth_client = APIKeyAuthClient(api_key=API_KEY, base_url=BASE_URL, organization=ORGANIZATION)
     return auth_client
 
-auth_client = user_authorize_carto()
+def repopulate_carto_dataset(table_name,rows):
+    """This (incomplete) function could be used to delete all rows from a 
+    Carto dataset and then add new ones."""
+    auth_client = user_authorize_carto()
 
-sql = sql_thing.SQLClient(auth_client)
+    sql = sql_thing.SQLClient(auth_client)
 
-query = "select * from {}".format(table_name)
-r = sql.send(query)
+    query = "select * from {}".format(table_name)
+    r = sql.send(query)
 
-delete_all = 'delete from {}'.format(table_name)
-sql.send(delete_all)
+    delete_all = 'delete from {}'.format(table_name)
+    sql.send(delete_all)
 
 
-insertions = []
+    insertions = []
 
-for row in rows:
-    insertion = "insert into {} ({}) VALUES ({})".format()
-    insertions.append(insertion)
+    for row in rows:
+        insertion = "insert into {} ({}) VALUES ({})".format()
+        insertions.append(insertion)
 
-from sql_thing import BatchSQLClient
+    from sql_thing import BatchSQLClient
 
-batchSQLClient = BatchSQLClient(auth_client)
-createJob = batchSQLClient.create(insertions)
+    batchSQLClient = BatchSQLClient(auth_client)
+    createJob = batchSQLClient.create(insertions)
 
-print(createJob['job_id'])
-
-# Then you can keep checking until the job is done.
-
+    print(createJob['job_id'])
+    
+    # Then you can keep checking until the job is done.
+    return createJob['job_id']
